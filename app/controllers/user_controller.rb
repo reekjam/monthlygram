22a4eaa9be2467e6ac30b15ab037996d7c86035b
Rick Rieta <rrieta@biddingforgood.com>
2013-07-31 17:47:39 -0400

    cleaned up the @today for /feed

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,15 +5,15 @@ class UserController < ApplicationController
 	def feed
 		client = Instagram.client(:access_token => session[:access_token])
 		@ig_user = client.user
-		@today_year = Date.today.strftime("%Y")
-		@today_month = Date.today.strftime("%m")
+		@today = [Date.today.strftime("%Y"), Date.today.strftime("%m")]
+		
 
 		if params[:month]
 			@photos = client.user_recent_media(count: 50, min_timestamp: get_start_and_end_date(params[:year], params[:month])[:start], 
 								max_timestamp: get_start_and_end_date(params[:year], params[:month])[:end])
 		else
-			@photos = client.user_recent_media(count: 50, min_timestamp: get_start_and_end_date(@today_year, @today_month)[:start], 
-								max_timestamp: get_start_and_end_date(@today_year, @today_month)[:end])
+			@photos = client.user_recent_media(count: 50, min_timestamp: get_start_and_end_date(@today[0], @today[1])[:start], 
+								max_timestamp: get_start_and_end_date(@today[0], @today[1])[:end])
 		end
 		@photos = @photos.reverse
 
