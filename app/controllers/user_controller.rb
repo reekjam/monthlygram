8986b620a24110e2c10a15ac2ad790e7a17cc8a9
Rick Rieta <rrieta@biddingforgood.com>
2013-07-31 10:09:21 -0400

    fixed year bug on /feed

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,14 +5,15 @@ class UserController < ApplicationController
 	def feed
 		client = Instagram.client(:access_token => session[:access_token])
 		@ig_user = client.user
-		@today = Date.today.strftime("%m")
+		@today_year = Date.today.strftime("%Y")
+		@today_month = Date.today.strftime("%m")
 
 		if params[:month]
 			@photos = client.user_recent_media(count: 50, min_timestamp: get_start_and_end_date(params[:year], params[:month])[:start], 
 								max_timestamp: get_start_and_end_date(params[:year], params[:month])[:end])
 		else
-			@photos = client.user_recent_media(count: 50, min_timestamp: get_start_and_end_date(@today)[:start], 
-								max_timestamp: get_start_and_end_date(@today)[:end])
+			@photos = client.user_recent_media(count: 50, min_timestamp: get_start_and_end_date(@today_year, @today_month)[:start], 
+								max_timestamp: get_start_and_end_date(@today_year, @today_month)[:end])
 		end
 		@photos = @photos.reverse
 
