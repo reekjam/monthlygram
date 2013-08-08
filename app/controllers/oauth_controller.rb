daf72d514e7eae3082987dc1805f05684cf76889
Rick Rieta <rrieta@biddingforgood.com>
2013-08-08 16:57:52 -0400

    heroku

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -1,10 +1,10 @@
 class OauthController < ApplicationController
   def connect
-  	redirect_to Instagram.authorize_url(:redirect_uri => 'http://localhost:3000/oauth/callback')
+  	redirect_to Instagram.authorize_url(:redirect_uri => 'http://monthlygram.heroku.com/oauth/callback')
   end
 
   def callback
-  	response = Instagram.get_access_token(params[:code], :redirect_uri => 'http://localhost:3000/oauth/callback')
+  	response = Instagram.get_access_token(params[:code], :redirect_uri => 'http://monthlygram.heroku.com/oauth/callback')
   	session[:access_token] = response.access_token
   	redirect_to user_feed_path
   end
